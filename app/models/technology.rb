1307a69f5c8b3e1ca00cba361b443e3f4ff2d865
matir8 <martin.markov28@gmail.com>
2018-02-23 11:05:11 +0200

    Implemented custom registration

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,5 +1,5 @@
 class Technology < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 256 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 256 }
 
   has_and_belongs_to_many :users
   has_and_belongs_to_many :posts
