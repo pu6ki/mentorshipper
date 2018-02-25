459d9ed735ef484665843ee3005d01a61857ebec
wencakisa <wencakisa@gmail.com>
2018-02-25 21:33:24 +0200

    Added technologies index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
+  root to: 'technologies#index'
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :posts
