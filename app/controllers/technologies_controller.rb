7f307c9225863b78fd008452e50e414cfdfe204a
wencakisa <wencakisa@gmail.com>
2018-02-27 15:13:35 +0200

    Style changes in questions views

--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -2,6 +2,6 @@ class TechnologiesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @technologies = Technology.all
+    @technologies = current_user.technologies
   end
 end
