f758097a59094df5c104c444d1b8d652355672db
wencakisa <wencakisa@gmail.com>
2018-02-28 12:03:26 +0200

    Questions filtering by technology is now a static method in model

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,7 +8,15 @@ class Question < ApplicationRecord
 
   has_many :answers
 
+  default_scope { order(created_at: :desc) }
+
   def solved?
     answers.select(&:solving).count > 0
+  end
+
+  class << self
+    def from_technology(technology_name)
+      select { |q| q.technology.name == technology_name }
+    end
   end
 end
