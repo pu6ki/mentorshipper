5f0a4d7f2fc00d63bf17d8ab0da70ec836cf5c46
matir8 <martin.markov28@gmail.com>
2018-03-14 00:10:27 +0200

    Temporary added url for importing the technologies

--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -2,4 +2,9 @@ class TechnologiesController < ApplicationController
   def index
     @technologies = current_user.technologies
   end
+
+  def create
+    @technology = Technology.create!(name: params[:name])
+    render json: @technology
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  post 'technologies' => 'technologies#create'
 end
