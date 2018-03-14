d7990e75c7ebafe72e863f8458ec5cf10ed55f97
matir8 <martin.markov28@gmail.com>
2018-03-14 13:07:31 +0200

    little fix

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,6 @@
 class PasswordsController < Devise::PasswordsController
   protected
   def after_sending_reset_password_instructions_path_for(resource_name)
-    sign_in_path
+    new_user_session
   end
 end
